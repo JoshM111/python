2b31d60ded6a0f301382807a5072b0febf31fc4a
Josh_M <josh.marson.12@gmail.com>
2020-10-01 17:33:35 -0700

    updates

--- lecture_project_2/lecture/views.py
+++ lecture_project_2/lecture/views.py
@@ -6,4 +6,13 @@ import bcrypt
 def index(request):
     return render(request, 'index.html')
 def register(request):
-    errors= User.objects.create_validator()
+    if request.method == "POST":
+        errors= User.objects.create_validator(requset.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.erro(request, value)
+            return redirect('/')
+        else:
+            pass
+            #create an account for User
+    return redirect('/')
